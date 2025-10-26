f9677b83e0ceb17d28909dfb81f24619b0df64a4
akins_dev <bobemmanuel503@gmail.com>
2025-10-26 10:52:49 +0100

    chore: integrate geneDetail fetch API

--- frontend/src/components/gene-viewer.tsx
+++ frontend/src/components/gene-viewer.tsx
@@ -1,6 +1,11 @@
-import type { GeneFromSearch } from "@/utils/genome-api";
+"use client";
+
+import { fetchGeneDetails, type GeneFromSearch } from "@/utils/genome-api";
 import { Button } from "./ui/button";
 import { ArrowLeft } from "lucide-react";
+import { useEffect, useState } from "react";
+import { set } from "zod/v4";
+import type { GeneBounds, GeneRecord } from "@/types/gene-details";
 
 const GeneViewer = ({
   gene,
@@ -11,6 +16,50 @@ const GeneViewer = ({
   genomeId: string;
   onClose: () => void;
 }) => {
+  const [geneBounds, setGeneBounds] = useState<GeneBounds | null>(null);
+  const [geneDetail, setGeneDetail] = useState<GeneRecord | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const [startPostion, setStartPosition] = useState<string>("");
+  const [endPosition, setEndPosition] = useState<string>("");
+
+  useEffect(() => {
+    const initializeGeneData = async () => {
+      setIsLoading(true);
+      setError(null);
+      setGeneDetail(null);
+      setStartPosition("");
+      setEndPosition("");
+
+      if (!gene.geneId) {
+        setError("Gene ID is missing, cannot fetch details.");
+        setIsLoading(false);
+        return;
+      }
+
+      try {
+        const { geneDetails, initialRange, geneBounds } = await fetchGeneDetails(gene.geneId);
+        setGeneDetail(geneDetails);
+        setGeneBounds(geneBounds);
+
+        if (initialRange) {
+          setStartPosition(initialRange.start.toString());
+          setEndPosition(initialRange.end.toString());
+
+          // fetch gene sequence
+        }
+
+
+
+      } catch (error) {
+        console.log("fetchGeneDetails error: ", error);
+        setError("Failed to fetch gene details. Please try again.");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+  }, [gene, genomeId]);
   return (
     <div className="space-y-6">
       <Button
